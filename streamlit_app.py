08cdae9346776266aa1c1a0d9f2fa1d30332e210
guillaumebachelot <79754947+guillaumebachelot@users.noreply.github.com>
2024-03-12 13:44:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 # Définition de la fonction de transformation
 def supprimer_colonnes_et_modifier_feuille(df):
+    # Liste des colonnes à supprimer
     colonnes_a_supprimer = [
         "Nom", 
         "Nom_de_naissance", 
@@ -23,26 +24,30 @@ def supprimer_colonnes_et_modifier_feuille(df):
     # Supprimer les colonnes spécifiées
     df.drop(columns=colonnes_a_supprimer, inplace=True, errors='ignore')
 
-    # Modifier le nom de la feuille
-    writer = pd.ExcelWriter("output.xlsx", engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name="Export MF")
-    writer.save()
+    return df
 
 # Interface Streamlit
-st.title("Transformation de fichier Excel")
+st.title("Transformation de fichier CSV en Excel")
 
-# Uploader de fichier Excel
-uploaded_file = st.file_uploader("Uploader un fichier Excel", type=["xlsx"])
+# Uploader de fichier CSV
+uploaded_file = st.file_uploader("Uploader un fichier CSV", type=["csv"])
 
 if uploaded_file is not None:
-    # Lecture du fichier Excel
-    df = pd.read_excel(uploaded_file)
+    # Lecture du fichier CSV
+    df = pd.read_csv(uploaded_file)
 
     # Bouton pour déclencher la transformation et le téléchargement
     if st.button("Transformer et télécharger"):
-        supprimer_colonnes_et_modifier_feuille(df)
+        df_transforme = supprimer_colonnes_et_modifier_feuille(df)
         st.success("La transformation est terminée. Téléchargez le fichier transformé ci-dessous.")
         
+        # Sauvegarder le DataFrame transformé dans un fichier Excel temporaire
+        with st.spinner("Enregistrement du fichier Excel..."):
+            temp_file_path = "temp.xlsx"
+            writer = pd.ExcelWriter(temp_file_path, engine='xlsxwriter')
+            df_transforme.to_excel(writer, index=False, sheet_name="Export MF")
+            writer.save()
+        
         # Bouton pour télécharger le fichier transformé
-        with open("output.xlsx", "rb") as file:
-            st.download_button(label="Télécharger le fichier transformé", data=file, file_name="output.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        with open(temp_file_path, "rb") as file:
+            st.download_button(label="Télécharger le fichier transformé", data=file, file_name="fichier_transforme.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
